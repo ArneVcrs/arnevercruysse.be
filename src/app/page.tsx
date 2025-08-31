1dd9913b7deebccfd673de56b83952699f5b5d1d
arne.vercruysse <arne.vercruysse@student.howest.be>
2025-09-01 00:30:51 +0200

    add content to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <div>
-      
+      <h1>Arne Vercruysse</h1>
+      <p>Hey there, thanks for stopping by! I&apos;m Arne.</p>
+      <p>*Note: this site has not been optimized for mobile screens yet, it is best viewed on desktop.</p>
+      <p>
+        This site is my personal little corner of the internet: a place where I can share my thoughts on my blog, 
+        showcase my portfolio, and expirement with new ideas. It&apos;s a work in progress, I will be adding more content as I find the time.
+      </p>
+      <p>Go take a look around and don&apos;t hesitate to get in touch!</p>
+      <p>The homepage is quite empty, for now...</p>
     </div>
   );
 }
