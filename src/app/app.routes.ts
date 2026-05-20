ee563cc07c03252f16ae9abb86d8452d7ee20c00
arne.vercruysse <arne.vercruysse@student.howest.be>
2026-05-20 19:14:27 +0200

    define application routes for Home, About, and Blog pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home/home-page.component';
+import { AboutPage } from './pages/about/about-page.component';
+import { BlogListPage } from './pages/blog/post-list/blog-list-page.component';
+import { BlogPostPage } from './pages/blog/post-detail/blog-post-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePage
+  },
+  {
+    path: 'about',
+    component: AboutPage
+  },
+  {
+    path: 'blog',
+    children: [
+      {
+        path: '',
+        component: BlogListPage
+      },
+      {
+        path: '**',
+        component: BlogPostPage
+      }
+    ]
+  }
+];
