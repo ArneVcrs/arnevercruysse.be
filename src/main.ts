a45a1a7fe48978981f50683c8d67cf773ce48fa1
arne.vercruysse <arne.vercruysse@student.howest.be>
2026-05-20 19:16:23 +0200

    register custom elements for Thumbnail and Image components

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { createCustomElement } from '@angular/elements';
+import { Thumbnail } from './app/components/thumbnail/thumbnail.component';
+import { Image } from './app/components/image/image.component';
 
 bootstrapApplication(App, appConfig)
+  .then((appRef) => {
+    const injector = appRef.injector;
+
+    const thumbnailEl = createCustomElement(Thumbnail, { injector });
+    customElements.define('app-thumbnail', thumbnailEl);
+
+    const imageEl = createCustomElement(Image, { injector });
+    customElements.define('app-image', imageEl);
+  })
   .catch((err) => console.error(err));
