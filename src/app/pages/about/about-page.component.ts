4b005db3189ec8925aaa91ea7eac1fae136354d4
arne.vercruysse <arne.vercruysse@student.howest.be>
2026-05-20 19:55:56 +0200

    add AboutPage and HomePage components

---
+++ src/app/pages/about/about-page.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from "@angular/core";
+
+@Component({
+  selector: "app-about-page",
+  templateUrl: "./about-page.component.html",
+  styleUrl: "./about-page.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AboutPage {}
