73808acc33699189b9ca110139e2b54a29c436d4
arne.vercruysse <arne.vercruysse@student.howest.be>
2026-05-22 15:54:43 +0200

    add blog service to manage blog posts retrieval

---
+++ src/app/services/blog.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { allBlogPosts, type BlogPost } from 'content-collections';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogService {
+  public getPosts(): Observable<BlogPost[]> {
+    return of(allBlogPosts.filter(post => post.isPublished));
+  }
+
+  public getPost(slug: string): Observable<BlogPost | undefined> {
+    return of(allBlogPosts.find(post => post.slug === slug));
+  }
+}
