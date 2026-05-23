19207494dc8dd4745edc270a75104dd9bfead133
arne.vercruysse <arne.vercruysse@student.howest.be>
2026-05-23 11:23:43 +0200

    add about page

--- src/app/pages/about/about-page.component.ts
+++ src/app/pages/about/about-page.component.ts
@@ -1,9 +1,33 @@
-import { Component, ChangeDetectionStrategy } from "@angular/core";
+import { Component, ChangeDetectionStrategy, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
 @Component({
   selector: "app-about-page",
   templateUrl: "./about-page.component.html",
   styleUrl: "./about-page.component.scss",
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AboutPage {}
+export class AboutPage {
+  public facts = [
+    "I'm 21 years old",
+    "I live in Belgium",
+    "I like cooking and gardening",
+    "I drink my coffee black",
+    "I speak Dutch, English, French, and basic German"
+  ];
+
+
+  public tools = [
+    { name: "Visual Studio Code - IDE", link: "https://code.visualstudio.com/" },
+    { name: "Obsidian - Notetaking", link: "https://obsidian.md/" },
+    { name: "Restic - Back-ups", link: "https://restic.readthedocs.io/en/stable/" },
+    { name: "Syncthing - File syncing", link: "https://syncthing.net/" },
+    { name: "Brave - Web Browser", link: "https://brave.com/" },
+    { name: "Todoist - Todo Lists", link: "https://www.todoist.com/" },
+    { name: "Zsh - Shell", link: "https://ohmyz.sh/" },
+    { name: "Tailscale - Remote Connection", link: "https://tailscale.com/" },
+    { name: "Bitwarden - Password Manager", link: "https://bitwarden.com/" },
+    { name: "Thunderbird - Email Client", link: "https://www.thunderbird.net/" }
+  ];
+}
+
